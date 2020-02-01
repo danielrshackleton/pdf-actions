7681d6c4e5bb414d901276dc16151928eea04658
danielrshackleton <daniel.shackleton2009@gmail.com>
2020-01-31 16:25:53 -0800

    Initial work on numpy 2D array

--- pdfFunctions/scanPDF.py
+++ pdfFunctions/scanPDF.py
@@ -1,23 +1,20 @@
-import sys
 import os
-from pdfminer.pdfparser import PDFParser
-from pdfminer.pdfdocument import PDFDocument
-from pdfminer.pdfpage import PDFPage, PDFTextExtractionNotAllowed
-from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+import numpy as np
+
 from pdfminer.pdfdevice import PDFDevice
+from pdfminer.pdfdocument import PDFDocument
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.pdfpage import PDFPage, PDFTextExtractionNotAllowed
+from pdfminer.pdfparser import PDFParser
 from pdfminer.pdftypes import resolve1
 
 
-def load_form(filename, password):
-    """Load pdf form contents into a nested list of name/value tuples"""
 
-    # get current directory
+def load_form(filename, password):
     filename = os.pardir + "/" + filename
 
     with open(filename, 'rb') as file:
         parser = PDFParser(file)
-
-        # for when PDF form has password restrictions
         if password is None:
             doc = PDFDocument(parser)
         else:
@@ -34,20 +31,36 @@ def load_form(filename, password):
             interpreter.process_page(page)
 
         fields = resolve1(doc.catalog['AcroForm'])['Fields']
+        pdf_table = np.array([], str)
+        _table_row = np.array
         for i in fields:
-            field = resolve1(i)
-            name, value = field.get('T'), field.get('V')
-            pair = '{0}: {1}'.format(name, value)
-            print(pair)
+            _table_row = _decode_fields(i)
+            np.concatenate((pdf_table, _table_row))
+
+        print(pdf_table)
+
+
+
+def _decode_fields(field_index):
+    field = resolve1(field_index)
+    name, value = _decode_decision(field.get('T', b''), '8'), _decode_decision(field.get('V', b''), '16')
+    print(f"{name}: {value}")
+    decoded_field = np.array([name, value])
+    return decoded_field
+
+
+def _decode_decision(value, decode):
+    if isinstance(value, bytes):
+        return value.decode(f'utf-{decode}')
+    else:
+        return value
+
+
 
 def main():
-    #myFile = sys.argv[1]
-    #myPass = sys.argv[2]
-
-    myFile = 'test.pdf'
-    myPass = None
-
-    load_form(myFile, myPass)
+    my_file = 'test.pdf'
+    my_pass = None
+    load_form(my_file, my_pass)
 
 
 if __name__ == "__main__":
