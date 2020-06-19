fff81547c3a4d7206e728a9f0884ce68885df529
danielrshackleton <43825001+danielrshackleton@users.noreply.github.com>
2020-06-19 15:27:19 -0700

    Modified docstrings

--- pdfFunctions/remove_page.py
+++ pdfFunctions/remove_page.py
@@ -6,7 +6,7 @@ import PyPDF2
 
 
 def _check_values(page_num, reader):
-    """ Checks that page_number is within pdf range (parses to int if 'start'/'end') """
+    """ Checks that page_number is within pdf range (also accepts 'start'/'end') """
 
     if page_num == 'start':
         page_num = 0
